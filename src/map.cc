f519d8ddca0b1bd92b73172562537c43c92fcd7b
Mike Burgess <mddburgess@users.noreply.github.com>
2017-06-17 13:37:03 -0700

    Episode 16: Adding Background Tiles

--- src/map.cc
+++ src/map.cc
@@ -18,6 +18,11 @@ Map* Map::createTestMap(Graphics& graphics) {
                     num_cols, Tile()
             )
     );
+    map->background_tiles_ = std::vector<std::vector<std::shared_ptr<Sprite>>>(
+            num_rows, std::vector<std::shared_ptr<Sprite>>(
+                    num_cols, std::shared_ptr<Sprite>()
+            )
+    );
 
     std::shared_ptr<Sprite> sprite = std::make_shared<Sprite>(
             graphics, "content/PrtCave.bmp",
@@ -32,6 +37,22 @@ Map* Map::createTestMap(Graphics& graphics) {
     map->tiles_[8][3] = tile;
     map->tiles_[7][2] = tile;
     map->tiles_[10][3] = tile;
+
+    std::shared_ptr<Sprite> chain_top = std::make_shared<Sprite>(
+            graphics, "content/PrtCave.bmp",
+            11 * Game::kTileSize, 2 * Game::kTileSize,
+            Game::kTileSize, Game::kTileSize);
+    std::shared_ptr<Sprite> chain_middle = std::make_shared<Sprite>(
+            graphics, "content/PrtCave.bmp",
+            12 * Game::kTileSize, 2 * Game::kTileSize,
+            Game::kTileSize, Game::kTileSize);
+    std::shared_ptr<Sprite> chain_bottom = std::make_shared<Sprite>(
+            graphics, "content/PrtCave.bmp",
+            13 * Game::kTileSize, 2 * Game::kTileSize,
+            Game::kTileSize, Game::kTileSize);
+    map->background_tiles_[8][2] = chain_top;
+    map->background_tiles_[9][2] = chain_middle;
+    map->background_tiles_[10][2] = chain_bottom;
 
     return map;
 }
@@ -64,6 +85,14 @@ void Map::update(int elapsed_time_ms) {
 
 void Map::drawBackground(Graphics& graphics) const {
     backdrop_->draw(graphics);
+    for (size_t row = 0; row < background_tiles_.size(); ++row) {
+        for (size_t col = 0; col < background_tiles_[row].size(); ++col) {
+            if (background_tiles_[row][col]) {
+                background_tiles_[row][col]->draw(
+                        graphics, col * Game::kTileSize, row * Game::kTileSize);
+            }
+        }
+    }
 }
 
 void Map::draw(Graphics& graphics) const {
